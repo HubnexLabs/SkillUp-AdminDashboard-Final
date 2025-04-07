64b251540e827423c9aebc4e16e59188a6015e96
ShubhamLiman <147237034+ShubhamLiman@users.noreply.github.com>
2025-04-07 17:30:45 +0530

    Update LoginForm.jsx

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+ilimport React from "react";
 import { useRef } from "react";
 import { NavLink } from "react-router-dom";
 import Cookies from "js-cookie";
@@ -55,13 +55,13 @@ function LoginForm({ setLogin }) {
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="Admin Id"
+                htmlFor="AdminEmail"
               >
                 Email
               </label>
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="Admin Id"
+                id="AdminEmail"
                 type="text"
                 placeholder="Admin Email"
                 ref={AdminRef}
